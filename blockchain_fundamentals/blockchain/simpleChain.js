029b5ed3c3cf64df3dc1f61748cd4953b707f21d
kirqz23 <kiryll.kuzniecow@gmail.com>
2022-12-21 21:17:11 +0100

    add blockchain

---
+++ blockchain_fundamentals/blockchain/simpleChain.js
@@ -0,0 +1,42 @@
+const SHA256 = require('crypto-js/sha256')
+
+/* ===== Block Class ===================================
+|  Class with a constructor for block data model       |
+|  ====================================================*/
+class Block {
+
+    constructor(data) {
+        this.hash = "";
+        this.height = 0;
+        this.body = data;
+        this.time = 0;
+        this.previousblockhash = "";
+    }
+}
+
+  
+  /* ===== Blockchain ===================================
+  |  Class with a constructor for blockchain data model  |
+  |  with functions to support:                          |
+  |     - createGenesisBlock()                           |
+  |     - getLatestBlock()                               |
+  |     - addBlock()                                     |
+  |     - getBlock()                                     |
+  |     - validateBlock()                                |
+  |     - validateChain()                                |
+  |  ====================================================*/
+class Blockchain{
+
+    constructor(){
+        this.chain = [];
+        this.addBlock(new Block("Genesis block"));
+    }
+
+    addBlock(newBlock){
+        if (this.chain.length > 0){
+            newBlock.previousblockhash = this.chain[this.chain.length-1].hash
+        }
+        newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
+        this.chain.push(newBlock);
+    }
+}
