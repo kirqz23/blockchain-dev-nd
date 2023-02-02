4aab3f0ec248b1f5592afa8c2af7b6833b50f99d
kirqz23 <kiryll.kuzniecow@gmail.com>
2023-02-02 14:09:22 +0100

    add refund functionality

--- blockchain_architecture/lemonade_stand/Lemonade.sol
+++ blockchain_architecture/lemonade_stand/Lemonade.sol
@@ -5,7 +5,7 @@ contract LemonadeStand {
     address owner;
     uint skuCount;
 
-    enum State { ForSale, Sold }
+    enum State { ForSale, Sold, Shipped }
 
     struct Item {
         string name;
@@ -20,6 +20,7 @@ contract LemonadeStand {
 
     event ForSale(uint skuCount);
     event Sold(uint sku);
+    event Shipped(uint sku);
 
     modifier onlyOnwer(){
         require(msg.sender == owner);
@@ -46,6 +47,14 @@ contract LemonadeStand {
         _;
     }
 
+    modifier makeRefund(uint _sku) {
+        _;
+        price = items[_sku].price;
+        if (msg.value - price > 0) {
+            items[sku].buyer.transfer(msg.value - price);
+        }
+    }
+
     constructor() public {
         owner = msg.sender;
         skuCount = 0;
@@ -64,12 +73,33 @@ contract LemonadeStand {
         });
     }
 
-    function buyItem(uint sku) forSale(sku) paidEnough(items[sku].price) public payable {
+    function buyItem(uint sku) forSale(sku) paidEnough(items[sku].price) makeRefund(sku) public payable {
         address buyer = msg.sender;
         uint price = items[sku].price;
         items[sku].buyer = buyer;
         items[sku].state = State.Sold;
         items[sku].seller.transfer(price);
         emit Sold(sku);
+    }
+
+    function fetchItem(uint _sku) public view returns (string name, uint sku, uint price, string stateIs, address seller, address buyer) {
+        uint state;
+        name = items[_sku].name;
+        sku = items[_sku].sku;
+        price = items[_sku].price;
+        state = uint(items[_sku].state);
+        if (state == 0){
+            stateIs = "For Sale";
+        }
+        if (state == 1){
+            stateIs = "Sold";
+        }
+        seller = items[_sku].seller;
+        buyer = items[_sku].buyer;
+    }
+
+    function shipItem(uint _sku) sold(_sku) verifyCaller(items[_sku].seller) public {
+        items[_sku].state = State.Shipped;
+        emit Shipped(_sku);
     }
 }
