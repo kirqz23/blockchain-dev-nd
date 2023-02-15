fab63f399c27e89d36b990ed4d6c09e411f20697
kirqz23 <kiryll.kuzniecow@gmail.com>
2023-02-15 16:19:57 +0100

    prepare for goerli deploy

--- blockchain_architecture/project_3/project-6/truffle.js
+++ blockchain_architecture/project_3/project-6/truffle.js
@@ -1,9 +1,23 @@
+
+const fs = require('fs');
+const HDWallet = require('./app/node_modules/truffle-hdwallet-provider');
+const infuraKey = fs.readFileSync("../infura.secret").toString().trim();
+const mnemonic = fs.readFileSync("../mnemonic.secret").toString().trim();
+
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
-    }
+    },
+    goerli: {
+      provider: () => new HDWallet(mnemonic, `https://goerli.infura.io/v3/${infuraKey}`),
+      network_id: 5,
+      gas: 4500000,
+      gasPrice: 10000000000,
+    },
+
   }
 };
