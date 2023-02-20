8e78cd43f2b7b4450eebc596db25ac6fb78e64a7
kirqz23 <kiryll.kuzniecow@gmail.com>
2023-02-20 18:51:25 +0100

    add reentrancy modifier

--- dapp/send_funds/contracts/Withdraw.sol
+++ dapp/send_funds/contracts/Withdraw.sol
@@ -6,13 +6,34 @@ contract Withdraw {
     using SafeMath for uint256;
 
     address private contractOwner;
-    mapping (address => uint256) private sales;
+    mapping(address => uint256) private sales;
+    uint256 private enabled = block.timestamp;
+    uint256 private counter = 1;
 
     constructor() public {
         contractOwner = msg.sender;
     }
 
-    function safeWithdraw(uint256 amount) external {
+    // rate limit to not allow calling a function too frequent
+    modifier rateLimit(uint256 time) {
+        require(block.timestamp >= enabled, "Rate limiting in effect");
+        enabled = enabled.add(time);
+        _;
+    }
+
+    // re-entrancy guard for your function
+    modifier entrancyGuard() {
+        counter = counter.add(1);
+        uint256 guard = counter;
+        _;
+        require(guard == counter, "That is not allowed");
+    }
+
+    function safeWithdraw(uint256 amount)
+        external
+        rateLimit(30 minutes)
+        entrancyGuard
+    {
         // Checks
         // check if the sender is externally owned account not contract
         require(msg.sender == tx.origin, "Contracts not allowed");
