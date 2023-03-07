ba1143d43bf95e7e4191fe4fdb326149f79a60e1
kirqz23 <kiryll.kuzniecow@gmail.com>
2023-03-07 21:58:07 +0100

    migration

--- dapp/upgradability/migrations/1_initial_migration.js
+++ dapp/upgradability/migrations/1_initial_migration.js
@@ -1,5 +1,9 @@
 var Migrations = artifacts.require("Migrations");
+var ExerciseC6C = artifacts.require("ExerciseC6C");
+var ExerciseC6CApp = artifacts.require("ExerciseC6CApp");
 
 module.exports = function(deployer) {
     deployer.deploy(Migrations);
+    deployer.deploy(ExerciseC6C);
+    deployer.deploy(ExerciseC6CApp);
 };
